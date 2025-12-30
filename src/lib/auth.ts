857fff2cdb45d5f78dc883cdb38ae21ce2b71efc
mrashed21 <rashedjaman768@gmail.com>
2025-12-31 02:32:08 +0600

    add google login

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -156,4 +156,12 @@ export const auth = betterAuth({
       }
     },
   },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      accessType: "offline",
+      prompt: "select_account consent",
+    },
+  },
 });
