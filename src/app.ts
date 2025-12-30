0e3cf3e1acc15466769b7e334c8ce0aabbcb0a34
mrashed21 <rashedjaman768@gmail.com>
2025-12-31 00:08:43 +0600

    add better auth

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
+import { toNodeHandler } from "better-auth/node";
 import express from "express";
+import { auth } from "./lib/auth";
 import router from "./router/router";
-
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/v1", router);
+app.all("/api/auth/*", toNodeHandler(auth));
+
 app.get("/", (req, res) => {
   res.send("blog server is running");
 });
---
+++ src/lib/auth.ts
@@ -0,0 +1,13 @@
+// import { PrismaClient } from "@generated/prisma/client";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+});
