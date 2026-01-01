3ec916f2c3496d36abd6e9ad53eb3f496062b4b0
mrashed21 <rashedjaman768@gmail.com>
2026-01-02 00:39:53 +0600

    add middleware

--- src/modules/post/post.route.ts
+++ src/modules/post/post.route.ts
@@ -1,7 +1,19 @@
-import express from "express";
+import { auth } from "@/lib/auth";
+import express, { NextFunction, Request, Response } from "express";
 import { postController } from "./post.controller";
 const router = express.Router();
 
-router.get("/", postController.gellAllPost);
+const authMiddleWare = (...role: any) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const session = await auth.api.getSession({
+      headers: req.headers as any,
+    });
+
+    console.log(session);
+    next();
+  };
+};
+
+router.get("/", authMiddleWare("admin", "user"), postController.gellAllPost);
 router.post("/", postController.createPost);
 export const postRouter = router;
