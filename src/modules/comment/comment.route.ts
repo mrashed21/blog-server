aa810c3606d767858169e2bb1a07b7aa8c1e5ae0
mrashed21 <rashedjaman768@gmail.com>
2026-01-05 00:00:16 +0600

    add auth middleware

--- src/modules/comment/comment.route.ts
+++ src/modules/comment/comment.route.ts
@@ -1,9 +1,14 @@
+import authMiddleWare, { UserRole } from "@/middleware/authMiddleWare";
 import express from "express";
 import { commentController } from "./comment.controller";
 
 const router = express.Router();
 
 // create comment
-router.post("/", commentController.createComment);
+router.post(
+  "/",
+  authMiddleWare(UserRole.user, UserRole.admin),
+  commentController.createComment
+);
 
 export const commentRouter = router;
