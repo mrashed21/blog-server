e94176babe24d7f161819fa0769b95a4c9f8dd28
mrashed21 <rashedjaman768@gmail.com>
2026-01-04 23:56:19 +0600

    create comment bussines logic

--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -1,9 +1,16 @@
+import { prisma } from "@/lib/prisma";
+
 const createComment = async (payload: {
   content: string;
   authorId: string;
   postId: string;
   parentId: string;
-}) => {};
+}) => {
+  const result = await prisma.comment.create({
+    data: payload,
+  });
+  return result;
+};
 
 export const commentService = {
   createComment,
