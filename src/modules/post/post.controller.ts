5cc9b16e84a1bd498fb4f5f05de8dd7c95be417e
mrashed21 <rashedjaman768@gmail.com>
2026-01-06 23:59:34 +0600

    get issue

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -101,7 +101,6 @@ const getPostById = async (req: Request, res: Response) => {
 const getMyPosts = async (req: Request, res: Response) => {
   try {
     const user = req.user;
-    console.log(user);
     const result = await postService.getMyPosts(user?.id!);
     res.status(200).json({
       success: true,
